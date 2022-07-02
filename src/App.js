cb182251063c405cc4b6e611a730e75b4c70307e
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-02 10:40:07 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Bookings from './components/bookings';
 import Home from './components/home';
 import Login from './components/login';
 import Navbar from './components/navbar';
+import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home/>
-      <Login/>
-      <Bookings/>
+      <Routes>
+        <Route path='/' element ={<Home/>}></Route>
+        <Route path='/login' element ={<Login/>}></Route>
+        <Route path='/bookings' element ={<Bookings/>}></Route>
+      </Routes>
     </div>
   );
 }
