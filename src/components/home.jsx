d75a80abf7d6c739658e328a2f5e32365eed33b1
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-02 11:20:12 +0530

    homepage except image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,24 +1,40 @@
 import React from "react";
-
+import styled from 'styled-components'
 function Home(){
-    React.useInsertionEffect(()=>{
+const [movie , setMovie] = React.useState([]);
+    React.useEffect(()=>{
         fetch("http://localhost:8080/movies")
         .then((response)=>{
             return response.json();
         })
         .then((response)=>{
-            console.log(response);
+            setMovie(response);
         })
         .catch((error)=>{
             console.log(error);
         })
     },[])
+    console.log(movie);
     return(
-        <>
-            Home Page
-        </>
+        <Container>
+             {
+                movie?.map((product)=>
+                (
+                    <div key ={product.id}>
+                    <>{product.title}</>
+                    <>{product.image}</>
+                    {/* <><Link to={`/products/${product.id}`}>More Details</Link></> */}
+                    </div>
+ 
+                ))
+            }
+        </Container>
     )
 }
 
 
 export default Home;
+let Container = styled.div`
+    display : grid;
+    grid-template-column: repeat(3,1fr);
+`
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-
+import styled from 'styled-components'
 function Navbar(){
     return(
-        <>
+        <Nav>
             <Link to = "/">Home</Link>
             <Link to = "/bookings">Bookings</Link>
             <Link to = "/login">Login</Link>
 
-        </>
+        </Nav>
     )
 }
-
-
 export default Navbar;
+
+const Nav = styled.div`
+        display:flex;
+        flex-direction : row;
+        justify-content : space-between;
+        padding : 10px;
+        font-size : 23px;
+        border : 1px solid black;
+        background-color : whitesmoke;
+        border-radius: 8px;
+        font-style : none;
+        color : black;
+`
